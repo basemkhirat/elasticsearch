c2e37d87f3bc7823b7bb1d8bc61838b6a132f2d0
basemkhirat <basemkhirat@gmail.com>
2017-02-12 17:13:36 +0200

    add testcase

---
+++ tests/ESTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ESTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testES()
+    {
+        $this->assertTrue(true);
+    }
+}
