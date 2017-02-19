eba806cd54831630c9e5654deed61eb126558a8e
basemkhirat <basemkhirat@gmail.com>
2017-02-19 22:51:27 +0200

    fix some compatibility isses

--- src/Pagination.php
+++ src/Pagination.php
@@ -3,6 +3,7 @@
 namespace Basemkhirat\Elasticsearch;
 
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\UrlWindow;
 
 class Pagination extends LengthAwarePaginator
 {
@@ -24,5 +25,35 @@ class Pagination extends LengthAwarePaginator
 
         require dirname(__FILE__) . "/pagination/" . $view . ".php";
     }
+
+
+    /**
+     * Get the array of elements to pass to the view.
+     *
+     * @return array
+     */
+    protected function elements()
+    {
+        $window = UrlWindow::make($this);
+
+        return array_filter([
+            $window['first'],
+            is_array($window['slider']) ? '...' : null,
+            $window['slider'],
+            is_array($window['last']) ? '...' : null,
+            $window['last'],
+        ]);
+    }
+
+    /**
+     * Determine if the paginator is on the first page.
+     *
+     * @return bool
+     */
+    public function onFirstPage()
+    {
+        return $this->currentPage() <= 1;
+    }
+
 
 }
