69c94151f5d25b9c2481232a1022d58d91b5a526
Moritz Friedrich <moritz@matchory.com>
2021-02-12 11:32:55 +0100

    Fixed test trait

--- tests/Traits/ESQueryTrait.php
+++ tests/Traits/ESQueryTrait.php
@@ -1,8 +1,20 @@
 <?php
+/** @noinspection PhpUnhandledExceptionInspection */
 
 namespace Matchory\Elasticsearch\Tests\Traits;
 
+use Elasticsearch\Client;
+use Matchory\Elasticsearch\Connection;
 use Matchory\Elasticsearch\Query;
+use PHPUnit\Framework\InvalidArgumentException;
+use PHPUnit\Framework\MockObject\ClassAlreadyExistsException;
+use PHPUnit\Framework\MockObject\ClassIsFinalException;
+use PHPUnit\Framework\MockObject\DuplicateMethodException;
+use PHPUnit\Framework\MockObject\InvalidMethodNameException;
+use PHPUnit\Framework\MockObject\OriginalConstructorInvocationRequiredException;
+use PHPUnit\Framework\MockObject\ReflectionException;
+use PHPUnit\Framework\MockObject\RuntimeException;
+use PHPUnit\Framework\MockObject\UnknownTypeException;
 
 /**
  * Class ESQueryTrait
@@ -57,10 +69,25 @@ trait ESQueryTrait
      * ES query object
      *
      * @return Query
+     * @throws InvalidArgumentException
+     * @throws ClassAlreadyExistsException
+     * @throws ClassIsFinalException
+     * @throws DuplicateMethodException
+     * @throws InvalidMethodNameException
+     * @throws OriginalConstructorInvocationRequiredException
+     * @throws ReflectionException
+     * @throws RuntimeException
+     * @throws UnknownTypeException
      */
     protected function getQueryObject(): Query
     {
-        return (new Query())
+        return (new Query(
+            new Connection(
+                $this->getMockBuilder(Client::class)
+                     ->disableOriginalConstructor()
+                     ->getMock()
+            )
+        ))
             ->index($this->index)
             ->type($this->type)
             ->take($this->take)
